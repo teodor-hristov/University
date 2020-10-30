34390a6afa06f368cb8640cfadc5226e403edc6c
Teodor Hristov <tedotedo771@gmail.com>
2020-10-30 13:36:48 +0200

    add task 8

--- Introduction to programming (UP)/Semester 1/03-OperatorsAndBitwiseOprations/seminar.cpp
+++ Introduction to programming (UP)/Semester 1/03-OperatorsAndBitwiseOprations/seminar.cpp
@@ -92,8 +92,36 @@ int main() {
     // }
 
     // cout << n << endl;
-    // // 00000..001
-    // // 11111..111
+    
+
+    //Task 7 
+
+    // int n;
+
+    // cin >> n;
+    // // 000..101 5
+    // // 111..010
+    // cout << n ^    
+
+    //Task 8
+    // int n;
+
+    // cin >> n;
+
+    // if(n & 1) 
+    //     cout << "Studeno e!" << endl;
+
+    // if(n & (1 << 1))
+    //     cout << "Vali snqg!" << endl;
+
+    // if(n & (1 << 2))
+    //     cout << "Vali dyjd!" << endl;
+
+    // if(n & (1 << 3))
+    //     cout << "Vetrovito e!" << endl;
+
+    // if(n & (1 << 4))
+    //     cout << "Slynchevo e!" << endl;
 
 
 }
