dd9f158292ebfdf8b7eb8fea81939ae358c48960
Teodor Hristov <tedotedo771@gmail.com>
2020-10-30 13:16:17 +0200

    add task 6

--- Introduction to programming (UP)/Semester 1/03-OperatorsAndBitwiseOprations/seminar.cpp
+++ Introduction to programming (UP)/Semester 1/03-OperatorsAndBitwiseOprations/seminar.cpp
@@ -75,7 +75,25 @@ int main() {
 
     //Task 6
 
-    
+    // int n;
+    // bool status = 0;
+
+    // cin >> n;
+
+    // for(int i = 0; i < 32; i++) {
+
+    //     if(status) {
+    //         n = n ^ (1 << i);
+    //     }
+
+    //     if(n & (1 << i)) {
+    //         status = 1;
+    //     }
+    // }
+
+    // cout << n << endl;
+    // // 00000..001
+    // // 11111..111
 
 
 }
