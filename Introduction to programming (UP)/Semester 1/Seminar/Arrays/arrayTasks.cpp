213d0cb6187fb0e8713fdf63a73d4f44884aff8c
Teodor Hristov <tedotedo771@gmail.com>
2020-11-13 17:15:36 +0200

    add task 2

--- Introduction to programming (UP)/Semester 1/Seminar/Arrays/arrayTasks.cpp
+++ Introduction to programming (UP)/Semester 1/Seminar/Arrays/arrayTasks.cpp
@@ -4,28 +4,59 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-void printNumbers(int);
+void insertNumbers(unsigned n);
+void printNumbers(const int array[], unsigned length);
+int findMinimum(unsigned n);
 
 int main() {
+
 #pragma region task1
+
+    // int n;
+    // cin >> n;
+    // insertNumbers(n);
+
+#pragma endregion task1
+
+#pragma region task2
+    
     int n;
     cin >> n;
-    printNumbers(n);
-#pragma ednregion task1
+    cout << findMinimum(n) << endl;
+
+#pragma endregion task2
 }
 
-void printNumbers(const int n) {
+int findMinimum(unsigned n) {
+    int temp = 0;
+    int min;
+    
+    cin >> min;
+    
+    for (unsigned i = 1; i < n; i++)
+    {
+        cin >> temp;
+        if(min > temp) {
+            min = temp;
+        }
+    }
+    
+    return min;
+}
+
+void insertNumbers(const unsigned n) {
     int array[n];
 
-    for (int i = 0; i < n; i++)
+    for (unsigned i = 0; i < n; i++)
     {
         cin >> array[i];
-    }
+    }  
+    printNumbers(array, n);
+}
 
-    for (int i = 0; i < n; i++)
+void printNumbers(const int array[], unsigned length) {
+    for (unsigned i = 0; i < length; i++)
     {
         cout << array[i] << endl;
     }
-    
-    
 }
