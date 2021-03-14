a2ba70f0b4ef66b057c735c17ab3cc8bc87031a2
Teodor Hristov <tedotedo771@gmail.com>
2021-03-14 21:56:03 +0200

    add task 3

--- OOP/Practice/09.03/Task 2/main.cpp
+++ OOP/Practice/09.03/Task 2/main.cpp
@@ -10,6 +10,7 @@ private:
     int experience;
     int soldCars;
 public:
+    Person() {};
     Person(char* name, char* surname, char* egn, int exp, int soldCars);
     ~Person();
 
@@ -43,6 +44,18 @@ public:
     void setSoldCars(int value){
         soldCars = value;
     }
+
+    void printPersonInfo() {
+        cout << "Name: " <<this->getName() << " " << " Surname: " <<this->getSurname() << " " << " EGN " <<this->getEgn() << " " <<" EXP " <<this->getExp() << "  " <<" Sold cars " << this->getSoldCars() << endl;
+    }
+
+    bool equalTo(Person &person){
+        if(this->getEgn() == person.getEgn()){
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 };
 
 Person::Person(char* name, char* surname, char* egn, int exp, int soldCars) {
@@ -63,6 +76,7 @@ private:
     double price;
     double mileage;
 public:
+    Car();
     Car(char* brand, char* model, int year, double price, double mileage);
     ~Car();
     char* getBrand(){
@@ -80,8 +94,25 @@ public:
     double getMileage(){
         return mileage;
     }
-};
 
+    void printCar(){
+        cout << this->brand << " " << this->model << " " << this->price << endl;
+    }
+
+    bool eaqualTo(Car secondCar) {
+        if(this->brand == secondCar.brand
+        && this->model == secondCar.model
+        && this->price == secondCar.price
+        && this->mileage == secondCar.mileage
+        && this->yearOfCreation == secondCar.yearOfCreation)
+        {
+            return 1;
+        }else {
+            return 0;
+        }
+    }
+};
+Car::Car() {};
 Car::Car(char *brand, char *model, int year, double price, double mileage) {
     this->brand = brand;
     this->model = model;
@@ -98,15 +129,29 @@ private:
     Person owner;
     Person* staff;
     Car* cars;
+    int carCount;
+    int workersCount;
 public:
-    Dealership(const Person &owner, Person *staff, Car *cars) :
+    Dealership(Person &owner) :
     owner(owner){
+        cout << owner.getName() << " created dealership. " << endl;
         this->owner = owner;
-        this->staff = staff;
-        this->cars = cars;
+        carCount = 0;
+        workersCount = 0;
+    }
+
+    void setWorkersCount(int workersCount) {
+        this->workersCount = workersCount;
+    }
+
+    int getWorkersCount() const {
+        return workersCount;
     }
 
     ~Dealership() {
+        if(this->getCars()) {
+            delete[] this->getCars();
+        }
     }
 
     Person getOwner(){
@@ -118,7 +163,127 @@ public:
     Car* getCars(){
         return cars;
     }
+    void setCars(Car* cars){
+        this->cars = cars;
+    }
+
+    int getCarCount() {
+        return this->carCount;
+    }
+
+    void setCarCount(int num) {
+        this->carCount = num;
+    }
+
+    void printCars() {
+        for (int i = 0; i < this->getCarCount(); ++i) {
+            this->cars[i].printCar();
+        }
+    }
+
+    void setStaff(Person *staff) {
+        Dealership::staff = staff;
+    }
+
+    void printWorkers(){
+        for(int i = 0; i < this->workersCount; i++) {
+            this->getStaff()[i].printPersonInfo();
+        }
+    }
+
+    void changeOwner(Person &newOwner) {
+        this->owner = newOwner;
+        cout << "Owner changed to " << newOwner.getName() << endl;
+    }
+
+    void sortWorkers(){
+        for (int i = 0; i < this->getWorkersCount(); ++i) {
+            for (int j = i; j < this->getWorkersCount(); ++j) {
+               if((double)this->getStaff()[i].getSoldCars() / (double)this->getStaff()[i].getExp() <
+               (double)this->getStaff()[j].getSoldCars() / (double)this->getStaff()[j].getExp()) {
+                   swap(this->getStaff()[i], this->getStaff()[j]);
+               }
+            }
+        }
+    }
+
+    void sortCars(){
+        for (int i = 0; i < this->getCarCount(); ++i) {
+            for (int j = i; j < this->getCarCount(); ++j) {
+                if((double)this->getCars()[i].getPrice() / (double)this->getCars()[i].getMileage() <
+                   (double)this->getCars()[j].getPrice() / (double)this->getCars()[j].getMileage()) {
+                    swap(this->getCars()[i], this->getCars()[j]);
+                }
+            }
+        }
+    }
+
 };
+
+void addCar(Dealership &dealer, Car &car) {
+    Car* newCarArray = new Car[dealer.getCarCount() + 1];
+    for (int i = 0; i < dealer.getCarCount(); ++i) {
+        newCarArray[i] = dealer.getCars()[i];
+    }
+    newCarArray[dealer.getCarCount()] = car;
+    if(dealer.getCarCount()){
+        delete[] dealer.getCars();
+    }
+    dealer.setCarCount(dealer.getCarCount() + 1);
+    dealer.setCars(newCarArray);
+}
+
+void sellCar(Dealership &dealer, Car &car, Person &seller) {
+    size_t tempCounter = 0;
+    Car* newCarArray = new Car[dealer.getCarCount() - 1];
+    for (int i = 0; i < dealer.getCarCount(); ++i) {
+        if(!dealer.getCars()[i].eaqualTo(car)) {
+            newCarArray[tempCounter] = dealer.getCars()[i];
+            tempCounter++;
+        }
+    }
+    if(dealer.getCars()){
+        delete[] dealer.getCars();
+    }
+    dealer.setCarCount(dealer.getCarCount() - 1);
+    dealer.setCars(newCarArray);
+    seller.setSoldCars(seller.getSoldCars() + 1);
+    cout << seller.getName() << " sold ";
+    car.printCar();
+    cout << endl;
+}
+
+void addWorker(Dealership &dealer, Person &worker) {
+    Person* newWorkerArr = new Person[dealer.getWorkersCount() + 1];
+    for (int i = 0; i < dealer.getWorkersCount(); ++i) {
+        newWorkerArr[i] = dealer.getStaff()[i];
+    }
+    newWorkerArr[dealer.getWorkersCount()] = worker;
+    if(dealer.getWorkersCount()){
+        delete[] dealer.getStaff();
+    }
+    dealer.setWorkersCount(dealer.getWorkersCount() + 1);
+    dealer.setStaff(newWorkerArr);
+}
+
+void removeWorker(Dealership &dealer, Person &worker) {
+    size_t tempCounter = 0;
+    Person* newWorkerArr = new Person[dealer.getWorkersCount() - 1];
+    for (int i = 0; i < dealer.getWorkersCount(); ++i) {
+        if(!dealer.getStaff()[i].equalTo(worker)) {
+            newWorkerArr[tempCounter] = dealer.getStaff()[i];
+            tempCounter++;
+        }
+    }
+    if(dealer.getStaff()){
+        delete[] dealer.getStaff();
+    }
+    dealer.setWorkersCount(dealer.getWorkersCount() - 1);
+    dealer.setStaff(newWorkerArr);
+
+    cout << worker.getName() << " has been fired." << endl;
+
+}
 
 int main() {
     Person worker1 = Person("Teodor", "Hristov", "01123123", 12, 4);
@@ -127,11 +292,28 @@ int main() {
     Car car1 = Car("Honda", "Civic", 1992, 1400, 200000);
     Car car2 = Car("VW", "Passat", 1989, 950.5, 10000000);
 
-    Person staff[2] = {worker1, worker2};
-    Car cars[2] = {car1, car2};
 
-    Dealership sofiaAuto = Dealership(owner,staff, cars);
+    Dealership sofiaAuto = Dealership(owner);
+    addCar(sofiaAuto,car2);
+    addCar(sofiaAuto, car1);
+    cout << "Cars currently active in dealership Sofia Auto:" << endl;
+    sofiaAuto.printCars();
+    sellCar(sofiaAuto, car2, worker2);
+    sofiaAuto.printCars();
 
-    //TODO: finish the task
+    addWorker(sofiaAuto, worker1);
+    addWorker(sofiaAuto, worker2);
+    cout << "Workers in Sofia Auto: " << endl;
+    sofiaAuto.printWorkers();
+    removeWorker(sofiaAuto, worker1);
+    sofiaAuto.printWorkers();
+
+    sofiaAuto.sortWorkers();
+    cout << "Workers sorted." << endl;
+    sofiaAuto.printWorkers();
+
+    sofiaAuto.sortCars();
+    cout << "Cars sorted." << endl;
+    sofiaAuto.printCars();
 
 }
