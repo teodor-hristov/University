1eebe4db14e71793fb54c728ab34146dc65ba798
teodor-hristov <tedotedo771@gmail.com>
2021-04-03 15:32:57 +0300

    remove all variable definitions

--- Object Oriented programming/Hw1/Vehicle/Vehicle.hpp
+++ Object Oriented programming/Hw1/Vehicle/Vehicle.hpp
@@ -4,8 +4,8 @@
 
 class Vehicle{
 private:
-    MyString regNumber = MyString();
-    MyString desc = MyString();
+    MyString regNumber;
+    MyString desc;
     std::size_t parkingSize;
 public:
     Vehicle(const char* registration, const char* description, std::size_t space);
