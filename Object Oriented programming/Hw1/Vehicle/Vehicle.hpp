0c4f48b0fcf1149cdcf7eba0a0b97c093146ccf8
teodor-hristov <tedotedo771@gmail.com>
2021-04-03 15:21:54 +0300

    add Vehicle header

---
+++ Object Oriented programming/Hw1/Vehicle/Vehicle.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "../MyString/MyString.hpp"
+
+class Vehicle{
+private:
+    MyString regNumber = MyString();
+    MyString desc = MyString();
+    std::size_t parkingSize;
+public:
+    Vehicle(const char* registration, const char* description, std::size_t space);
+    const char* registration() const;
+    const char* description() const;
+    std::size_t space() const;
+
+};
