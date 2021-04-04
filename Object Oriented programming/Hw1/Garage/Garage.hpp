c73356c59052bf2dd4cd7cfe20a2054a85f2dbe6
teodor-hristov <tedotedo771@gmail.com>
2021-04-04 23:10:17 +0300

    add Garage.hpp

---
+++ Object Oriented programming/Hw1/Garage/Garage.hpp
@@ -0,0 +1,25 @@
+#include "../Vehicle/Vehicle.hpp"
+
+class Garage{
+private:
+    std::size_t spots;
+    std::size_t usedSpots;
+    std::size_t lastIndex;
+    Vehicle** vhcls;
+public:
+    Garage(std::size_t size); //max parking spots - size
+    Garage(const Garage &garage);
+    ~Garage();
+    Garage& operator=(Garage &garage);
+    void insert(Vehicle& v);
+    void erase(const char* registration);
+    Vehicle& at(std::size_t pos);
+    const Vehicle& at(std::size_t pos) const;
+    Vehicle& operator[](std::size_t pos);
+    const Vehicle& operator[](std::size_t pos) const;
+    bool empty() const;
+    std::size_t size() const;
+    void clear();
+    Vehicle* find(const char* registration);
+    const Vehicle* find(const char* registration) const;
+};
