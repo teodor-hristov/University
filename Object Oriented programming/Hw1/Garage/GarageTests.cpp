8a187fc0039ffd418bba24f795b7e84c4ab5f63e
teodor-hristov <tedotedo771@gmail.com>
2021-04-04 23:10:29 +0300

    add tests

---
+++ Object Oriented programming/Hw1/Garage/GarageTests.cpp
@@ -0,0 +1,62 @@
+#pragma once
+#include "Garage.hpp"
+#include "../catch_amalgamated.hpp"
+
+bool areCarsEqual(const Vehicle &v1, const Vehicle &v2){
+    return (v1.space() == v2.space()) && !strcmp(v1.registration(), v2.registration()) && !strcmp(v1.description(), v2.description());
+}
+
+TEST_CASE("Copy ctor"){
+    Garage garage1 = Garage(7);
+    Vehicle* car = new Vehicle("Algebra", "will tell you", 1);
+    garage1.insert(*car);
+
+    Garage garage2 = garage1;
+
+    REQUIRE(garage2.size() == garage1.size());
+    REQUIRE(areCarsEqual(garage2[0],garage1[0]));
+
+    delete car;
+}
+
+TEST_CASE("Test Garage.insert() with normal values"){
+    Garage garage = Garage(2);
+    Vehicle* vhcl = new Vehicle("asdsad", "asdsad", 2);
+
+    garage.insert(*vhcl);
+
+    REQUIRE(areCarsEqual(garage[0], *vhcl));
+    delete vhcl;
+}
+
+TEST_CASE("Test Garage.insert() multiple vehicles"){
+    Garage garage = Garage(2);
+    Vehicle* vhcl = new Vehicle("asdsad", "asdsad", 2);
+    Vehicle* car = new Vehicle("car", "asdsad", 2);
+
+    garage.insert(*vhcl);
+
+    REQUIRE_THROWS(garage.insert(*car));
+
+    delete car;
+    delete vhcl;
+}
+
+TEST_CASE("Assignment operator with normal values"){
+    Garage garage1 = Garage(5);
+    Garage garage2 = Garage(2);
+
+    Vehicle* car1 = new Vehicle("Quick", "brown", 2);
+    Vehicle* car2 = new Vehicle("Quick", "White", 1);
+
+    garage1.insert(*car1);
+    garage1.insert(*car2);
+
+    garage2 = garage1;
+
+    REQUIRE(garage1.size() == garage2.size());
+
+    for (int i = 0; i < 3; ++i) {
+        REQUIRE(areCarsEqual(garage2[i], garage1[i]));
+    }
+}
