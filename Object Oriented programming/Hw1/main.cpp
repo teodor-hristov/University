ea4e31906da60ef4b2dd7d933d19639194ee7226
teodor-hristov <tedotedo771@gmail.com>
2021-04-16 01:41:00 +0300

    update main

--- Object Oriented programming/Hw1/main.cpp
+++ Object Oriented programming/Hw1/main.cpp
@@ -2,22 +2,15 @@
 #include "MyString/MyString.hpp"
 #include "Garage/Garage.hpp"
 #include "Vehicle/Vehicle.hpp"
+#include "VehicleAllocator/VehicleAllocator.hpp"
 
 using namespace std;
 
 void printGarage(Garage &garage) {
-    const char* desc;
-    const char* reg;
 
     for (int i = 0; i < garage.getLastIndex(); ++i) {
-        reg = garage[i].registration();
-        desc = garage[i].description();
-
-        cout << "Id: " << i <<"\nDesc: " << desc << "\nRegistration: " << reg << "\nSize: "
+        cout << "Id: " << i <<"\nDesc: " << garage[i].description() << "\nRegistration: " << garage[i].registration() << "\nSize: "
              << garage[i].space() << endl;
-
-        delete reg;
-        delete desc;
     }
 }
 
@@ -25,39 +18,19 @@ void printMenu() {
     cout << "Choose an option:\n1.Add car to garage.\n2.Remove car from garage.\n3.Print garage.\n4.Esc\n";
 }
 
-void addCar(Garage &garage) {
-    int spots;
-    char registration[128], description[128];
-    Vehicle *vhcl;
-
-    cout << "Enter registration:" << endl;
-    cin.ignore();
-    cin.getline(registration, 128);
-
-    cout << "Enter description:" << endl;
-    cin.ignore();
-    cin.getline(description, 128);
-
-    cout << "Enter car size:" << endl;
-    cin >> spots;
-
-    vhcl = new Vehicle(registration, description, spots);
-
+void addCar(VehicleAllocator &vhclAlloc, Garage &garage) {
+    Vehicle* vhcl = vhclAlloc.createVehicle(garage);
     garage.insert(*vhcl);
 }
 
 void removeCar(Garage &garage) {
     int index;
-    const char* reg;
     cout << "Choose car: " << endl;
 
     printGarage(garage);
 
     cin >> index;
-    reg = garage[index].registration();
-    garage.erase(reg);
-
-    delete reg;
+    garage.erase(garage[index].registration());
 }
 
 
@@ -66,6 +39,7 @@ int main() {
     cout << "Garage size: " << endl;
     cin >> garageSize;
     Garage garage = Garage(garageSize);
+    VehicleAllocator vhcls = VehicleAllocator();
 
     int option;
     while (1) {
@@ -73,7 +47,7 @@ int main() {
         cin >> option;
         switch (option) {
             case 1:
-                addCar(garage);
+                addCar(vhcls,garage);
                 break;
             case 2:
                 removeCar(garage);
@@ -86,6 +60,7 @@ int main() {
 
                 return 0;
         }
+
     }
 
 }
